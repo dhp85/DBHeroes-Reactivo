cf5cdc2a0e51a73ca5af91b24b2b8dd0c6ccfb18
Diego Herreros <diegohp85@gmail.com>
2024-11-23 15:07:50 +0100

    feat: creation folders and structure app.

---
+++ DBHeroes-Reactivo/SupportFiles/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  DBHeroes-Reactivo
+//
+//  Created by Diego Herreros Parron on 23/11/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+      
+        guard let scene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: scene)
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+    }
+}
+
